4a34e67debc08b4cca02a8f1d18ace7f87f1f2af
mathanki <mathanki091suren@gmail.com>
2025-01-15 16:04:36 -0500

    Lesson 4 changes

---
+++ GuidedLab305.4.1A/src/main/java/org/example/controller/DeletingUser.java
@@ -0,0 +1,22 @@
+package org.example.controller;
+
+import org.example.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class DeletingUser {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        User u = new User();
+        u.setId(3);
+        session.remove(u);
+        tx.commit();
+        session.close();
+        factory.close();
+    }
+
+}
